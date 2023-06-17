1c15e1a79d4b4497bb5a4eb7bf75a6c45feb6fa4
Onotuamen <onosa.itua@icloud.com>
2023-06-17 16:27:31 -0700

    print f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+	*main - A program that prints a line using printf
+	*Descirption: 'why do i always have to put a description'
+	*Return: always 0
+*/
+
+	int main(void)
+	{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+	}
+
