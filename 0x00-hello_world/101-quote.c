ff62a00d5f08672fd5e1cfdcaa5467a47a84b01c
Onotuamen <onosa.itua@icloud.com>
2023-06-17 19:23:22 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,12 @@
 #include <unistd.h>
 /**
  *main - Entry point
- *Description - 'print a script but dont use printf to stderr'
+ *Description - 'print script but not printf to stderr'
  *Return: always 0
  */
 	int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout)
+		;
 	return (1);
 }
