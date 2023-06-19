801e4f02433ac4f43f9bdbff7cfc90ccaabbc01c
Onotuamen <onosa.itua@icloud.com>
2023-06-19 16:50:25 -0700

    pos or neg int

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,8 +11,8 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-        int n;
-	
+	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
@@ -22,11 +22,11 @@ int main(void)
 	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
-	}	
+	}
 	else
 	{
 		printf("%d is negative\n", n);
 	}
 
 	return (0);
-} 
+}
