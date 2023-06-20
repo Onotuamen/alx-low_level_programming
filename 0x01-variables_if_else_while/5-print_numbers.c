22fe722e38a27963cad85fadefb04b5e405b9d93
Onotuamen <onosa.itua@icloud.com>
2023-06-19 19:11:55 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+	*main - Entry point
+	*Desription: 'print lowercase alphets'
+	*Return: always 0
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+
+	return (0);
+}
+
