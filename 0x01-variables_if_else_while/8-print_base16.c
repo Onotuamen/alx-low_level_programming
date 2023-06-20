6b374f0dc6ead035ce5610a2b29be3c796d9585a
Onotuamen <onosa.itua@icloud.com>
2023-06-19 19:38:18 -0700

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Desription: 'print lowercase alphets'
+*Return: always 0
+*/
+
+int main(void)
+{
+	int n;
+	int a;
+
+	for (n = 48; <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (a = 97; a <= 102; m++)
+	{
+		putchar(a);
+	}
+	putchar('\n')
+
+	return (0);
+
+}
