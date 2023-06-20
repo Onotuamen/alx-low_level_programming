cd880e6a62530aea3fbe41d2391404c9549e5753
Onotuamen <onosa.itua@icloud.com>
2023-06-19 20:49:24 -0700

    100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+	*main - Entry point
+	*Desription: 'print lowercase alphets'
+	*Return: always 0
+*/
+
+int(void)
+
+	{
+		int a;
+
+		for (a = 0; a < 10; a++)
+		{
+			putchar (a + '0');
+			if (a < 9)
+			{
+			putchar(',');
+			putchar(' ');
+
+		}
+		}
+	putchar ('\n');
+	return (0);
+	}
