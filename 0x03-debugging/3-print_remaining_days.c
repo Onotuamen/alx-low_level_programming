caf56edc3131f218a6daf896a3d8be2c952939ba
John Doe <johndoe@example.com>
2023-08-17 18:24:27 -0700

    3-main

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -6,27 +6,26 @@
  * are reamining in a year taking leap year into account
  * @month: month in numnber format
  * @day: day of the year
- * @year: specific year 
+ * @year: specific year
  * Return: void
  */
 
-void print_remainng_days(int day, int month, int year)
+void print_remaining_days(int day, int month, int year)
 {
-	if (( year % 4 == 0) && (year % 400 == 0 || year % 100 != 0))
+	if ((year % 4 == 0) && (year % 400 == 0 || year % 100 != 0))
 	{
 	if (month >= 3 && >= 60)
 	{
 	day++;
 	}
-	
 	printf("Day of the year: %d\n", day);
 	printf("Remaining days: %d\n", 366 - day);
 	}
 	else
 	{
-	if (month ==2 && == 60)
+	if (month == 2 && == 60)
 	{
-	printf("invalid date: %02d/%02d/%04d\n", month, day -31, year);
+	printf("invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 	}
 	else
 	{
