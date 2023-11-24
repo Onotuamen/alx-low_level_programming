2c38c3bd3b24d6e25cc3bfe76c7fc20f1ab8af14
Onotuamen <onosa.itua@icloud.com>
2023-11-23 20:21:37 -0800

    corrected runtime error

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,7 +5,7 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
-void _print(char *str, int 1);
+void _print(char *str, int num);
 #include <stdio.h>
 #include <stdlib.h>
 int _putchar(char c);
