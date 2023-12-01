e0a7cf1534e7cc4fea5ecbb642811c7d05f12a89
Onotuamen <onosa.itua@icloud.com>
2023-12-01 06:04:57 -0800

    refactored code variadic functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,10 +3,10 @@
 	#include <stdio.h>
 
 
-	/**
-	 * print_all - prints anything
-	 * @format: list of types of arguments passed to the function
-	 */
+/**
+* print_all - prints anything
+* @format: list of types of arguments passed to the function
+*/
 	void print_all(const char * const format, ...)
 	{
 		int i = 0;
